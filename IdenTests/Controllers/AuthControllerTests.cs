00ab9efac62da2ad95ef14cddbf41c3a38a8d4f8
AzubuikeOkolie <okolieazubuike@gmail.com>
2024-07-07 14:37:33 +0100

    units tests

---
+++ IdenTests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Iden.Controllers.Tests
+{
+    [TestClass()]
+    public class AuthControllerTests
+    {
+        [TestMethod()]
+        public void AuthControllerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
